85c92c029559dd36f4ca1f4d595c9eedf891f39d
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2021-12-13 18:11:26 -0500

    current forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,88 @@
 
+var btnSubmit = document.querySelector(".submit");
+var userInput = document.querySelector("#userInput");
+var currentForecast = document.querySelector(".current-forecast");
+myKey = "54b595bf3d71cfd4083a4f576940e6e9"
 
 
+
+var getUviIndex = function(lon,lat) {
+    apiUrl = " https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" +lon +"&appid=" + myKey +"&exclude=daily,minutely,hourly";
+    
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var dataTemp = data.current.uvi;
+                var tempClass;
+                var uvi  =  document.createElement("p");
+                var s = document.createElement("span");
+                if(dataTemp <= 2){
+               
+                    tempClass = "bg-success";
+                }
+
+                else if(6> dataTemp && dataTemp > 2){
+                    tempClass = "bg-warning"
+                }
+
+                else if(8> dataTemp && dataTemp> 5){
+                    //custom class to set the background to orange
+                    s.classList.add("bg-orange");
+                }
+
+                else{
+                    tempClass = "bg-alert";
+                }
+                
+                s.classList.add(tempClass);
+                s.setAttribute("style","display:inline-block;width:30px;text-align:center;border-radius:30%");
+                s.textContent = dataTemp;
+                uvi.textContent ="UV Index: ";
+                uvi.appendChild(s);
+                currentForecast.appendChild(uvi);
+            })
+            
+        }
+        else{
+            console.log("error on uvi")
+        }
+    })
+
+ 
+}
 
 var apiCall = function(city){
-    myKey = "54b595bf3d71cfd4083a4f576940e6e9"
+   
     apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid="+myKey+"&units=metric";
 
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                var temp = data.main.temp;
-                // console.log(data);
-                // console.log("Temp: ",temp);
-                // console.log("lat: ", data.coord.lat);
-                // console.log("lon: ",data.coord.lon);
-                // console.log("Name: ",data.name);
-                // console.log("date: ", data.dt);
+                console.log(data);
 
+                //create name variable
+                var name = document.createElement("h2");
+                name.classList.add("font-weight-bold");
+                name.textContent = data.name;
 
+                //create temp variable
+                var temp = document.createElement("p");
+                temp.textContent = "Temp: " +Math.floor(data.main.temp )+ " °C";
+               
+                //create humidity variable
+                var humidity = document.createElement("p");
+                humidity.textContent = "Humidity: " +data.main.humidity + " %";
+                
+                //create wind variable
+                var wind = document.createElement("p");
+                wind.textContent = "Wind: " +data.wind.speed + " MPH";
+
+                //create UV index
+                var lon = data.coord.lon;
+                var lat = data.coord.lat;
+                getUviIndex(lon, lat);
+
+                currentForecast.append(name,temp,wind,humidity);
 
             })
         } else {
@@ -27,5 +92,20 @@ var apiCall = function(city){
 }   
 
 
-apiCall("santander");
+
+
+
+var getUserInput = function(event){
+    var city = userInput.value.trim();
+    if(city){
+        userInput.value = "";
+        apiCall(city);
+    }
+    else{
+        alert("Please enter a valid input")
+    }
+   // addToHistory(city);
+
+}
+btnSubmit.addEventListener("click",getUserInput);
 
